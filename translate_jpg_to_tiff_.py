9067272b3037e576fe04e0c7c92083fc86fd421d
lululelu <lululelu@users.noreply.github.com>
2016-03-31 15:46:59 +0800

    separate .py for file conversion

--- translate_jpg_to_tiff_.py
+++ translate_jpg_to_tiff_.py
@@ -1,5 +1,6 @@
 
 # Converting Slave image in jpg to tif
+# Input path of jpg slave image
 img = Image.open('C:\Users\EnviSAGE ResLab\Desktop\DPAD\Xy to Latlong\Butuan.Slave.02.jpg')
 img.save('C:\Users\EnviSAGE ResLab\Desktop\DPAD\Xy to Latlong\Butuan.Slave.02.tiff')
 
